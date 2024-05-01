086a3da2ca2e2defeef3a62cfd8f22422335356c
unknown <andysh317@gmail.com>
2024-05-01 19:16:59 +0530

    Java automation

---
+++ src/main/java/com/sagility/automation/App.java
@@ -0,0 +1,13 @@
+package com.sagility.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
